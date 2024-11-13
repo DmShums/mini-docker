d64aff41d0b0a53c2d5d42193d3bf7d3683bef8d
SahaidakYurii <sahaidak.pn@ucu.edu.ua>
2024-11-13 14:14:09 +0200

    minor change

--- src/cgroups.cpp
+++ src/cgroups.cpp
@@ -3,23 +3,23 @@
 
 #include "cgroups.hpp"
 
-void addCgroup(const std::string& cgroupName) {
-    std::string newCgroupPath = CGROUP_PATH + cgroupName;
+void addCgroup(const std::string& cgroupPath) {
+    std::string newCgroupPath = CGROUP_PATH + cgroupPath;
 
     if (mkdir(newCgroupPath.c_str(), 0755) != 0) {
-        std::cerr << "Failed to create cgroup "<< cgroupName << std::endl;
+        std::cerr << "Failed to create cgroup "<< cgroupPath << std::endl;
     }
 }
 
-void removeCgroup(const std::string& cgroupName) {
-    if (rmdir((CGROUP_PATH + cgroupName).c_str()) != 0) {
-        std::cerr << "Failed to remove cgroup "<< cgroupName << std::endl;
+void removeCgroup(const std::string& cgroupPath) {
+    if (rmdir((CGROUP_PATH + cgroupPath).c_str()) != 0) {
+        std::cerr << "Failed to remove cgroup "<< cgroupPath << std::endl;
     }
 }
 
-void addProcessToCgroup(const std::string& cgroupName, pid_t pid) {
+void addProcessToCgroup(const std::string& cgroupPath, pid_t pid) {
 //    creates new process
-    std::ofstream procsFile(CGROUP_PATH + cgroupName + "/cgroup.procs");
+    std::ofstream procsFile(CGROUP_PATH + cgroupPath + "/cgroup.procs");
     if (!procsFile) {
         std::cerr << "Failed to open cgroup.procs. Check cgroup name" << std::endl;
         return;
@@ -28,9 +28,9 @@ void addProcessToCgroup(const std::string& cgroupName, pid_t pid) {
     procsFile.close();
 }
 
-void addController(const std::string& cgroupName, const std::string& controller){
+void addController(const std::string& cgroupPath, const std::string& controller){
 //    adds controller to cgroup
-    std::ofstream controllersFile(CGROUP_PATH + cgroupName + "/cgroup.subtree_controllers");
+    std::ofstream controllersFile(CGROUP_PATH + cgroupPath + "/cgroup.subtree_controllers");
     if (!controllersFile) {
         std::cerr << "Failed to open cgroup.subtree_controllers. Check cgroup name" << std::endl;
         return;
@@ -39,9 +39,9 @@ void addController(const std::string& cgroupName, const std::string& controller)
     controllersFile.close();
 }
 
-void removeController(const std::string& cgroupName, const std::string& controller){
+void removeController(const std::string& cgroupPath, const std::string& controller){
 //    removes controller to cgroup
-    std::ofstream controllersFile(CGROUP_PATH + cgroupName + "/cgroup.subtree_controllers");
+    std::ofstream controllersFile(CGROUP_PATH + cgroupPath + "/cgroup.subtree_controllers");
     if (!controllersFile) {
         std::cerr << "Failed to open cgroup.subtree_controllers. Check cgroup name" << std::endl;
         return;
@@ -50,9 +50,9 @@ void removeController(const std::string& cgroupName, const std::string& controll
     controllersFile.close();
 }
 
-void setHardMemoryLimit(const std::string& cgroupName, const std::string& limit) {
+void setHardMemoryLimit(const std::string& cgroupPath, const std::string& limit) {
 //    adds RAM usage limit to cgroup with cgroupPath after which the process will be killed
-    std::ofstream memLimitFile(CGROUP_PATH + cgroupName + "/memory.max");
+    std::ofstream memLimitFile(CGROUP_PATH + cgroupPath + "/memory.max");
     if (!memLimitFile) {
         std::cerr << "Failed to open memory.max. Check if memory added to controllers" << std::endl;
         return;
@@ -61,9 +61,9 @@ void setHardMemoryLimit(const std::string& cgroupName, const std::string& limit)
     memLimitFile.close();
 }
 
-void setSwapLimit(const std::string& cgroupName, const std::string& limit){
+void setSwapLimit(const std::string& cgroupPath, const std::string& limit){
 //    adds Swap usage limit to cgroup with cgroupPath
-    std::ofstream memLimitFile(CGROUP_PATH + cgroupName + "/memory.swap.max");
+    std::ofstream memLimitFile(CGROUP_PATH + cgroupPath + "/memory.swap.max");
     if (!memLimitFile) {
         std::cerr << "Failed to open memory.max. Check if memory added to controllers" << std::endl;
         return;
@@ -72,14 +72,14 @@ void setSwapLimit(const std::string& cgroupName, const std::string& limit){
     memLimitFile.close();
 }
 
-void setCPUWeight(const std::string& cgroupName, uint weight){
+void setCPUWeight(const std::string& cgroupPath, uint weight){
 //    sets weight of the cGroup for CPU resources
     if (weight > 1000 || weight < 1){
         std::cerr << "Wrong weight value. The weight should be in range of [1;1000]" << std::endl;
         return;
     }
 
-    std::ofstream weightFile(CGROUP_PATH + cgroupName + "/cpu.weight");
+    std::ofstream weightFile(CGROUP_PATH + cgroupPath + "/cpu.weight");
     if (!weightFile) {
         std::cerr << "Failed to open cpu.weight. Check if cpu added to controllers" << std::endl;
     }
@@ -88,9 +88,9 @@ void setCPUWeight(const std::string& cgroupName, uint weight){
     weightFile.close();
 }
 
-void setCpuLimit(const std::string& cgroupName, const std::string& maxUsage, const std::string& period) {
+void setCpuLimit(const std::string& cgroupPath, const std::string& maxUsage, const std::string& period) {
 //    adds CPU time limitation timer resets after each period
-    std::ofstream cpuLimitFile(CGROUP_PATH + cgroupName +  "/cpu.max");
+    std::ofstream cpuLimitFile(CGROUP_PATH + cgroupPath +  "/cpu.max");
     if (!cpuLimitFile) {
         std::cerr << "Failed to open cpu.max. Check if cpu added to controllers" << std::endl;
         return;
@@ -99,8 +99,8 @@ void setCpuLimit(const std::string& cgroupName, const std::string& maxUsage, con
     cpuLimitFile.close();
 }
 
-void setPidLimit(const std::string& cgroupName, const size_t& limit){
-    std::ofstream  pidLimitFile(CGROUP_PATH + cgroupName + "/pids.max");
+void setPidLimit(const std::string& cgroupPath, const size_t& limit){
+    std::ofstream  pidLimitFile(CGROUP_PATH + cgroupPath + "/pids.max");
     if (!pidLimitFile) {
         std::cerr << "Failed to open pids.max. Check if pids added to controllers" << std::endl;
         return;
