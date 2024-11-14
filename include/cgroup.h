87c812637b2a87ace71d6f9ba3871e31aca823ed
SahaidakYurii <sahaidak.pn@ucu.edu.ua>
2024-11-14 12:52:15 +0200

    cGroup class

--- include/cgroup.h
+++ include/cgroup.h
@@ -12,24 +12,24 @@
 class CGroup{
 private:
     std::string cgroupPath;
+
+    void addController(const std::string& controller);
 public:
     CGroup() = delete;
     CGroup(const std::string& cgroupPath);
 
     void addProcess(pid_t pid);
+
     void setCpuLimit(const std::string& maxUsage, const std::string& period);
+
     void setHardMemoryLimit(const std::string& limit);
+    void setThrottlingMemoryLimit(const std::string& limit);
+    void setSwapLimit(const std::string& limit);
+
+    void setPidLimit(const size_t& limit);
 };
 
 
-
-void setThrottlingMemoryLimit(const std::string& cgroupPath, const std::string& limit);
-void setSwapLimit(const std::string& cgroupPath, const std::string& limit);
-void setPidLimit(const std::string& cgroupPath, const size_t& limit);
-void addCgroup(const std::string& cgroupPath);
-void addController(const std::string& cgroupPath, const std::string& controller);
-void removeController(const std::string& cgroupPath, const std::string& controller);
-void removeCgroup(const std::string& cgroupPath);
 
 
 #endif //MINI_DOCKER_CGROUP_H
--- src/cgroup.cpp
+++ src/cgroup.cpp
@@ -4,17 +4,29 @@
 #include "cgroup.h"
 
 CGroup::CGroup(const std::string& cgroupPath) {
+//    creates new CGroup with name cgroupPath and adds itself to the group
     std::string newCgroupPath = CGROUP_PATH + cgroupPath;
 
     if (mkdir(newCgroupPath.c_str(), 0755) != 0) {
         std::cerr << "Failed to create cgroup "<< cgroupPath << std::endl;
     }
+
+    addController("pid");
+    addController("memory");
+    addController("cpu");
+
+    addProcess(getpid());
 }
 
-void removeCgroup(const std::string& cgroupPath) {
-    if (rmdir((CGROUP_PATH + cgroupPath).c_str()) != 0) {
-        std::cerr << "Failed to remove cgroup "<< cgroupPath << std::endl;
+void CGroup::addController(const std::string& controller){
+//    adds controller to cgroup
+    std::ofstream controllersFile(CGROUP_PATH + cgroupPath + "/cgroup.subtree_controllers");
+    if (!controllersFile) {
+        std::cerr << "Failed to open cgroup.subtree_controllers. Check cgroup name" << std::endl;
+        return;
     }
+    controllersFile << ("+" + controller);
+    controllersFile.close();
 }
 
 void CGroup::addProcess(pid_t pid) {
@@ -28,26 +40,15 @@ void CGroup::addProcess(pid_t pid) {
     procsFile.close();
 }
 
-void addController(const std::string& cgroupPath, const std::string& controller){
-//    adds controller to cgroup
-    std::ofstream controllersFile(CGROUP_PATH + cgroupPath + "/cgroup.subtree_controllers");
-    if (!controllersFile) {
-        std::cerr << "Failed to open cgroup.subtree_controllers. Check cgroup name" << std::endl;
+void CGroup::setCpuLimit(const std::string& maxUsage, const std::string& period) {
+//    adds CPU time limitation timer resets after each period
+    std::ofstream cpuLimitFile(CGROUP_PATH + cgroupPath +  "/cpu.max");
+    if (!cpuLimitFile) {
+        std::cerr << "Failed to open cpu.max. Check if cpu added to controllers" << std::endl;
         return;
     }
-    controllersFile << ("+" + controller);
-    controllersFile.close();
-}
-
-void removeController(const std::string& cgroupPath, const std::string& controller){
-//    removes controller to cgroup
-    std::ofstream controllersFile(CGROUP_PATH + cgroupPath + "/cgroup.subtree_controllers");
-    if (!controllersFile) {
-        std::cerr << "Failed to open cgroup.subtree_controllers. Check cgroup name" << std::endl;
-        return;
-    }
-    controllersFile << ("-" + controller);
-    controllersFile.close();
+    cpuLimitFile << maxUsage << " " << period;
+    cpuLimitFile.close();
 }
 
 void CGroup::setHardMemoryLimit(const std::string& limit) {
@@ -61,7 +62,18 @@ void CGroup::setHardMemoryLimit(const std::string& limit) {
     memLimitFile.close();
 }
 
-void setSwapLimit(const std::string& cgroupPath, const std::string& limit){
+void CGroup::setThrottlingMemoryLimit(const std::string &limit) {
+//    adds RAM usage limit to cgroup with cgroupPath after which the process will throttle
+    std::ofstream memLimitFile(CGROUP_PATH + cgroupPath + "/memory.high");
+    if (!memLimitFile) {
+        std::cerr << "Failed to open memory.max. Check if memory added to controllers" << std::endl;
+        return;
+    }
+    memLimitFile << limit;
+    memLimitFile.close();
+}
+
+void CGroup::setSwapLimit(const std::string& limit){
 //    adds Swap usage limit to cgroup with cgroupPath
     std::ofstream memLimitFile(CGROUP_PATH + cgroupPath + "/memory.swap.max");
     if (!memLimitFile) {
@@ -72,34 +84,7 @@ void setSwapLimit(const std::string& cgroupPath, const std::string& limit){
     memLimitFile.close();
 }
 
-void setCPUWeight(const std::string& cgroupPath, uint weight){
-//    sets weight of the cGroup for CPU resources
-    if (weight > 1000 || weight < 1){
-        std::cerr << "Wrong weight value. The weight should be in range of [1;1000]" << std::endl;
-        return;
-    }
-
-    std::ofstream weightFile(CGROUP_PATH + cgroupPath + "/cpu.weight");
-    if (!weightFile) {
-        std::cerr << "Failed to open cpu.weight. Check if cpu added to controllers" << std::endl;
-    }
-
-    weightFile << weight;
-    weightFile.close();
-}
-
-void CGroup::setCpuLimit(const std::string& maxUsage, const std::string& period) {
-//    adds CPU time limitation timer resets after each period
-    std::ofstream cpuLimitFile(CGROUP_PATH + cgroupPath +  "/cpu.max");
-    if (!cpuLimitFile) {
-        std::cerr << "Failed to open cpu.max. Check if cpu added to controllers" << std::endl;
-        return;
-    }
-    cpuLimitFile << maxUsage << " " << period;
-    cpuLimitFile.close();
-}
-
-void setPidLimit(const std::string& cgroupPath, const size_t& limit){
+void CGroup::setPidLimit(const size_t& limit){
     std::ofstream  pidLimitFile(CGROUP_PATH + cgroupPath + "/pids.max");
     if (!pidLimitFile) {
         std::cerr << "Failed to open pids.max. Check if pids added to controllers" << std::endl;
